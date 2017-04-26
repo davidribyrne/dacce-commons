bb1f125f05e6fbb63c3bb003d04f577c4a99caf4
David Byrne <dbyrne@us.ibm.com>
2017-04-25 19:44:48 -0600

    fix?

--- commons-persistence/src/main/java/net/dacce/commons/persistence/PersonMapper.java
+++ commons-persistence/src/main/java/net/dacce/commons/persistence/PersonMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package net.dacce.commons.persistence;
 
 import org.apache.ibatis.annotations.*;
@@ -17,23 +16,3 @@ public interface PersonMapper
 			+ "(#{id}, #{name}, #{age})")
 	void insertPerson(Person person);
 }
-=======
-package net.dacce.commons.persistence;
-
-import org.apache.ibatis.annotations.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.util.*;
-
-public interface PersonMapper
-{
-	@Select("SELECT * FROM people where ID = #{ID}")
-	Person getPersonByID(int id);
-	
-	@Insert("INSERT INTO people "
-			+ "(id, name, age) "
-		+ "VALUES "
-			+ "(#{id}, #{name}, #{age})")
-	void insertPerson(Person person);
-}
->>>>>>> Stashed changes
--- commons-persistence/src/main/resources/schema.sql
+++ commons-persistence/src/main/resources/schema.sql
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 CREATE TABLE IF NOT EXISTS people (
 	id int PRIMARY KEY, 
 	name varchar(80),
 	age int
-=======
-CREATE TABLE IF NOT EXISTS people (
-	id int PRIMARY KEY, 
-	name varchar(80),
-	age int
->>>>>>> Stashed changes
 );
